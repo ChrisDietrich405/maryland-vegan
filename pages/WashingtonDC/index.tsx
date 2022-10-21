c4f9b1319c21b0c4c9fb49ac9392e30b63983825
Chris <chrisdietrich3666@gmail.com>
2022-10-21 12:37:19 -0500

    feat added restaurants

--- pages/WashingtonDC/index.tsx
+++ pages/WashingtonDC/index.tsx
@@ -2,7 +2,59 @@ import React from 'react'
 
 const WashingtonDC = () => {
   return (
-    <div>WashingtonDC</div>
+    <div>
+    <h3>Washington D.C.</h3>
+    <ul>
+      <li>
+        <a href="https://www.elizabethsgoneraw.com/" target="_blank">
+          Elizabeth's Gone Raw
+        </a>
+        <p>Address: 1341 L St NW Suite 1, Washington, DC </p>
+      </li>
+      <li>
+        <a href="https://www.fancyradishdc.com/" target="_blank">
+          Fancy Radish
+        </a>
+        <p>Address: 600 H St NE, Washington, DC 20002 </p>
+      </li>
+      <li>
+        <a href="https://vegz.us/" target="_blank">
+          VEGZ
+        </a>
+        <p>Address: 2120 18th St NW BSMT, Washington, DC 20009 </p>
+      </li>
+      <li>
+        <a href="https://www.fancyradishdc.com/" target="_blank">
+          Fancy Radish
+        </a>
+        <p>Address: 600 H St NE, Washington, DC 20002 </p>
+      </li>
+      <li>
+        <a href="https://www.fancyradishdc.com/" target="_blank">
+          Fancy Radish
+        </a>
+        <p>Address: 600 H St NE, Washington, DC 20002 </p>
+      </li>
+      <li>
+        <a href="https://www.fancyradishdc.com/" target="_blank">
+          Fancy Radish
+        </a>
+        <p>Address: 600 H St NE, Washington, DC 20002 </p>
+      </li>
+      <li>
+        <a href="https://www.fancyradishdc.com/" target="_blank">
+          Fancy Radish
+        </a>
+        <p>Address: 600 H St NE, Washington, DC 20002 </p>
+      </li>
+      <li>
+        <a href="https://www.fancyradishdc.com/" target="_blank">
+          Fancy Radish
+        </a>
+        <p>Address: 600 H St NE, Washington, DC 20002 </p>
+      </li>
+    </ul>
+  </div>
   )
 }
 
