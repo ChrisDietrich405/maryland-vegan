bd47874ceb5bd5fa4a3d6ddd2daa7a0f33d61bd8
Chris <chrisdietrich3666@gmail.com>
2022-10-22 14:22:15 -0500

    feat added restaurants and styled restaurants page

--- pages/WashingtonDC/index.tsx
+++ pages/WashingtonDC/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from "../../styles/restaurants.module.css"
 
 const WashingtonDC = () => {
   return (
-    <div>
-    <h3>Washington D.C.</h3>
+    <div className={styles.restaurant_container}>
+    <h2>Washington D.C.</h2>
     <ul>
       <li>
         <a href="https://www.elizabethsgoneraw.com/" target="_blank">
@@ -24,34 +25,34 @@ const WashingtonDC = () => {
         <p>Address: 2120 18th St NW BSMT, Washington, DC 20009 </p>
       </li>
       <li>
-        <a href="https://www.fancyradishdc.com/" target="_blank">
-          Fancy Radish
+        <a href="https://stickyfingersdiner.com/" target="_blank">
+          Sticky Fingers
         </a>
-        <p>Address: 600 H St NE, Washington, DC 20002 </p>
+        <p>Address: 406 H St NE, Washington, DC 20002 </p>
       </li>
       <li>
-        <a href="https://www.fancyradishdc.com/" target="_blank">
-          Fancy Radish
+        <a href="https://spacycloud.com/" target="_blank">
+          Spacy Cloud
         </a>
-        <p>Address: 600 H St NE, Washington, DC 20002 </p>
+        <p>Address: 2309 18th St NW, Washington, DC 20009</p>
       </li>
       <li>
-        <a href="https://www.fancyradishdc.com/" target="_blank">
-          Fancy Radish
+        <a href="https://fruitive.com/" target="_blank">
+          Fruitive
         </a>
-        <p>Address: 600 H St NE, Washington, DC 20002 </p>
+        <p>Address: 1094 Palmer Alley NW, Washington, DC 20001 </p>
       </li>
       <li>
-        <a href="https://www.fancyradishdc.com/" target="_blank">
-          Fancy Radish
+        <a href="https://www.ilovenuvegan.com/" target="_blank">
+          NuVegan Cafe
         </a>
-        <p>Address: 600 H St NE, Washington, DC 20002 </p>
+        <p>Address: 2928 Georgia Ave NW, Washington, DC 20001 </p>
       </li>
       <li>
-        <a href="https://www.fancyradishdc.com/" target="_blank">
-          Fancy Radish
+        <a href="https://shouk.com/" target="_blank">
+          Shouk
         </a>
-        <p>Address: 600 H St NE, Washington, DC 20002 </p>
+        <p>Address: 655 K St NW, Washington, DC 20001</p>
       </li>
     </ul>
   </div>
