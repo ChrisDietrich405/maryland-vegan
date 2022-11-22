c8d9876ba2151271ff2f2f1c0526720ea42e1ab0
Chris <chrisdietrich3666@gmail.com>
2022-11-22 18:51:42 -0500

    test

--- pages/BaltimoreCity.tsx/index.tsx
+++ pages/BaltimoreCity.tsx/index.tsx
@@ -1,134 +1,32 @@
-import React from 'react'
-import data from "../../restaurants.db.json"
+import React from "react";
+import data from "../../restaurants.db.json";
 
 const BaltimoreCity = () => {
-  console.log(data)
   return (
-    <div className="container">
-        <ul className="inner_container">
-           {data.baltimorecity.map((restaurant) => {
+    <>
+    <h1>Baltimore City Restaurants</h1>
+      <div className="container text-center my-4">
+        <div className="row">
+          {data.baltimorecity.map((restaurant) => {
             return (
-              <a target="_blank" href = {restaurant.url}>
-              <h3>{restaurant.name}</h3>
-              <p>{restaurant.address}</p>
-              </a>
-            )
-           })}
-            {/* <li>
-              <a
-                href="https://landofkush.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Land Of Kush
-              </a>
-              <p>Address: 840 N Eutaw St, Baltimore, MD 21201</p>
-            </li>
-            <li>
-              <a
-                href="https://www.thegreenerkitchen.com/?fbclid=IwAR1-M9QZk0iM2nCtgZYuqDlWtVTDKpK5LDFvLd4-NbPvVpd1tA2LIgo9tkw"
-                target="_blank"
-              >
-                The Greener Kitchen
-              </a>
-              <p>Address: 803 Washington Blvd, Baltimore, MD 21230</p>
-            </li>
-            <li>
-              <a href="https://dodahskitchen.com/" target="_blank">
-                Dodah's Kitchen
-              </a>
-              <p>Address: 1210 N. Charles St., Baltimore, MD 21201</p>
-            </li>
-            <li>
-              <a
-                href="https://www.matthewkenneycuisine.com/double-zero-baltimorehttps://www.matthewkenneycuisine.com/double-zero-baltimore"
-                target="_blank"
-              >
-                Double Zero
-              </a>
-              <p>Address: 414 Light St, Baltimore, MD 21202, United States</p>
-            </li>
-            <li>
-              <a href="https://www.gangstervegan.com/" target="_blank">
-                Gangster Vegan
-              </a>
-              <p>Address: 1065 S Charles St, Baltimore, MD 21230</p>
-              <img
-                src={"/GANGSTER_VEGAN.webp"}
-                alt="GANGSTER_VEGAN.png"
-                style={{ width: "84px", height: "66px", objectFit: "contain" }}
-              />
-            </li>
-            <li>
-              <a href="http://harmonybakery.net/" target="_blank">
-                Harmony Bakery
-              </a>
-              <p>Address: 3446 Chestnut Ave, Baltimore, MD 21211</p>
-            </li>
-            <li>
-              <a href="https://www.legendarylifestylecafe.com/" target="_blank">
-                Legendary Lifestyle Cafe
-              </a>
-              <p>
-                Address: 1301 North Broadway, Baltimore, Maryland, USA, 21213
-              </p>
-            </li>
-            <li>
-              <a
-                href="https://www.matthewkenneycuisine.com/liora"
-                target="_blank"
-              >
-                Liora
-              </a>
-              <p>
-                Address: The "Charm'tastic Mile, 414 Light St, Baltimore, MD
-                21202
-              </p>
-            </li>
-            <li>
-              <a href="https://mymamasvegan.com/" target="_blank">
-                My Mama's Vegan
-              </a>
-              <p>Address: 2915 Greenmount Ave, Baltimore, Maryland</p>
-            </li>
-            <li>
-              <a href="http://www.ilovenuvegan.com/" target="_blank">
-                NuVegan Cafe
-              </a>
-              <p>Address: 3105 St Paul St, Baltimore, MD</p>
-            </li>
-            <li>
-              <a href="https://www.refocusedvegan.com/" target="_blank">
-                Refocused
-              </a>
-              <p>Address: 429 N Eutaw St #1N, Baltimore, MD</p>
-            </li>
-            <li>
-              <a href="https://www.sproutvegan2.com/" target="_blank">
-                Sprout Vegan 2.0
-              </a>
-              <p>Address: 101 N Wolfe St suite 4, Baltimore, MD</p>
-            </li>
-            <li>
-              <a href="http://stemfarmkitchen.com/" target="_blank">
-                Stem Farm and Kitchen
-              </a>
-              <p>
-                Address: 301 W 29th St (at R. House food hall), Baltimore,
-                Maryland
-              </p>
-            </li>
-            <li>
-              <a href="https://www.greenhousejuicecafe.com/" target="_blank">
-                Green House Juice Cafe
-              </a>
-              <p>Address: 725 N Wolfe St. Baltimore, MD</p>
-            </li>
-            */}
-           </ul>
-          
-     </div>
-   )
- }
+              <div className="col-lg-4 col-md-6 col-sm-12 mb-5">
+                <div className="card" style={{ minHeight: "200px" }}>
+                  <a
+                    className="card-body"
+                    target="_blank"
+                    href={restaurant.url}
+                  >
+                    <h3 className="card-title">{restaurant.name}</h3>
+                    <p className="card-text">{restaurant.address}</p>
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
- export default BaltimoreCity 
+export default BaltimoreCity;
--- pages/Resources/index.tsx
+++ pages/Resources/index.tsx
@@ -17,170 +17,29 @@ const Resources = () => {
           content="Resources is dedicated to the non profits in Maryland working to make the world a more peaceful place"
         />
       </Head>
-      <div className="container text-center my-4">
-        <div className="row">
-          {data.resources.map((resource) => {
-            return (
-              <div className="col-lg-4 col-md-6 col-sm-12 mb-5" >
-                <div className="card" style={{minHeight: "200px"}}>
-                  <a className="card-body" target="_blank" href={resource.url}>
-                    <h5 className="card-title">{resource.name}</h5>
-                    <p className="card-text">{resource.content}</p>
-                  </a>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-
-      {/* <img src="..." className="card-img-top" alt="..." /> */}
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
-      <a href="#" className="btn btn-primary">
-        Go somewhere
-      </a>
+      <>
       <h1>Resources</h1>
-
-      <ul>
-        <li>
-          <a
-            href="https://www.afrovegansociety.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Afro Vegan Society
-          </a>
-          <p>
-            Afro-Vegan Society is a Baltimore based national nonprofit
-            organization with a mission to provide resources and support to help
-            people in marginalized communities transition to vegan living.
-          </p>
-        </li>
-        <li>
-          <a
-            href="https://www.animalsanctuary.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Poplar Spring Animal Sanctuary
-          </a>
-          <p>
-            Poplar Spring Animal Sanctuary is a 400 acre non-profit refuge in
-            Poolesville, Maryland for farm animals and wildlife.{" "}
-          </p>
-        </li>
-        <li>
-          <a
-            href="https://blackvegsociety.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Black Veg Society
-          </a>
-          <p>
-            The Black Veg Society’s mission is to educate the public,
-            particularly BIPOC communities, on the benefits of veganism and the
-            plant-based diet.
-          </p>
-        </li>
-        <li>
-          <a
-            href="https://earthsavebaltimore.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Earthsave Baltimore
-          </a>
-          <p>
-            Earthsave Baltimore is a local non profit dedicated to promoting all
-            aspects of veganism. It hosts potlucks, restaurant get togethers and
-            speaking events.
-          </p>
-        </li>
-
-        <li>
-          <a
-            href="https://www.vrg.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vegetarian Resource Group
-          </a>
-          <p>
-            Vegetarian Resource Group is a non profit dedicated to educating the
-            public about the benefits of a vegan lifestyle.
-          </p>
-        </li>
-        <li>
-          <a
-            href="https://www.facebook.com/groups/163345897614371"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Maryland Vegan Families
-          </a>
-          <p>
-            Maryland Vegan Families is a Facebook group dedicated to supporting
-            and providing resources to vegan families living in Maryland
-          </p>
-        </li>
-        <li>
-          <a
-            href="https://www.vrg.ohttps://www.facebook.com/groups/509056979150170/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Baltimore Vegans
-          </a>
-          <p>
-            Baltimore Vegans is a Facebook group dedicated to encouraging
-            veganism and supporting vegans in Baltimore
-          </p>
-        </li>
-        <li>
-          <a
-            href="https://www.pcrm.org/barnard-medical-center"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Barnard Medical Center
-          </a>
-          <p>
-            Barnard Medical Center is a 100% vegan medical center dedicated to
-            helping people improve their health
-          </p>
-        </li>
-        <li>
-          <a
-            href="https://www.vrg.ohttps://www.facebook.com/groups/509056979150170/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Baltimore Vegans
-          </a>
-          <p>
-            Baltimore Vegans is a Facebook group dedicated to encouraging
-            veganism and supporting vegans in Baltimore
-          </p>
-        </li>
-        <li>
-          <a
-            href="https://www.vrg.ohttps://www.facebook.com/groups/509056979150170/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Baltimore Vegans
-          </a>
-          <p>
-            Baltimore Vegans is a Facebook group dedicated to encouraging
-            veganism and supporting vegans in Baltimore
-          </p>
-        </li>
-      </ul>
+        <div className="container text-center my-4">
+          <div className="row">
+            {data.resources.map((resource) => {
+              return (
+                <div className="col-lg-4 col-md-6 col-sm-12 mb-5">
+                  <div className="card" style={{ minHeight: "200px" }}>
+                    <a
+                      className="card-body"
+                      target="_blank"
+                      href={resource.url}
+                    >
+                      <h5 className="card-title">{resource.name}</h5>
+                      <p className="card-text">{resource.content}</p>
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </>
     </>
   );
 };
